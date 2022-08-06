fa211bc245151653e3d209ca25dc62707ad785ff
Marian S <109340004+OrangeFoxi@users.noreply.github.com>
2022-08-06 12:43:08 +0200

    Add files via upload

---
+++ MainButtonViewer-beta1.0/MainButtonViewer-beta1.0/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace MainButtonViewer_beta1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelW = new System.Windows.Forms.Label();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelS = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
+            this.labelMBL = new System.Windows.Forms.Label();
+            this.labelMBR = new System.Windows.Forms.Label();
+            this.labelcpsl = new System.Windows.Forms.Label();
+            this.labelcpsr = new System.Windows.Forms.Label();
+            this.panelSpace = new System.Windows.Forms.Panel();
+            this.panelSpaceLine = new System.Windows.Forms.Panel();
+            this.timerCps = new System.Windows.Forms.Timer(this.components);
+            this.timerRGB = new System.Windows.Forms.Timer(this.components);
+            this.labelDebug = new System.Windows.Forms.Label();
+            this.comboBoxProcess = new System.Windows.Forms.ComboBox();
+            this.timer500 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelW
+            // 
+            this.labelW.BackColor = System.Drawing.Color.Gray;
+            this.labelW.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelW.Location = new System.Drawing.Point(688, 23);
+            this.labelW.Name = "labelW";
+            this.labelW.Size = new System.Drawing.Size(109, 23);
+            this.labelW.TabIndex = 0;
+            this.labelW.Text = "label1";
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Interval = 1;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // labelA
+            // 
+            this.labelA.BackColor = System.Drawing.Color.Gray;
+            this.labelA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelA.Location = new System.Drawing.Point(697, 92);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(100, 23);
+            this.labelA.TabIndex = 1;
+            this.labelA.Text = "label1";
+            // 
+            // labelS
+            // 
+            this.labelS.BackColor = System.Drawing.Color.Gray;
+            this.labelS.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelS.Location = new System.Drawing.Point(697, 69);
+            this.labelS.Name = "labelS";
+            this.labelS.Size = new System.Drawing.Size(100, 23);
+            this.labelS.TabIndex = 2;
+            this.labelS.Text = "label2";
+            // 
+            // labelD
+            // 
+            this.labelD.BackColor = System.Drawing.Color.Gray;
+            this.labelD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelD.Location = new System.Drawing.Point(697, 46);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(100, 23);
+            this.labelD.TabIndex = 3;
+            this.labelD.Text = "label3";
+            // 
+            // labelMBL
+            // 
+            this.labelMBL.BackColor = System.Drawing.Color.Gray;
+            this.labelMBL.ForeColor = System.Drawing.Color.Violet;
+            this.labelMBL.Location = new System.Drawing.Point(697, 115);
+            this.labelMBL.Name = "labelMBL";
+            this.labelMBL.Size = new System.Drawing.Size(100, 23);
+            this.labelMBL.TabIndex = 4;
+            this.labelMBL.Text = "label4";
+            // 
+            // labelMBR
+            // 
+            this.labelMBR.BackColor = System.Drawing.Color.Gray;
+            this.labelMBR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelMBR.Location = new System.Drawing.Point(697, 0);
+            this.labelMBR.Name = "labelMBR";
+            this.labelMBR.Size = new System.Drawing.Size(100, 23);
+            this.labelMBR.TabIndex = 5;
+            this.labelMBR.Text = "label5";
+            // 
+            // labelcpsl
+            // 
+            this.labelcpsl.BackColor = System.Drawing.Color.Gray;
+            this.labelcpsl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelcpsl.Location = new System.Drawing.Point(759, 153);
+            this.labelcpsl.Name = "labelcpsl";
+            this.labelcpsl.Size = new System.Drawing.Size(38, 15);
+            this.labelcpsl.TabIndex = 6;
+            this.labelcpsl.Text = "label1";
+            // 
+            // labelcpsr
+            // 
+            this.labelcpsr.BackColor = System.Drawing.Color.Gray;
+            this.labelcpsr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.labelcpsr.Location = new System.Drawing.Point(759, 138);
+            this.labelcpsr.Name = "labelcpsr";
+            this.labelcpsr.Size = new System.Drawing.Size(38, 15);
+            this.labelcpsr.TabIndex = 7;
+            this.labelcpsr.Text = "label1";
+            // 
+            // panelSpace
+            // 
+            this.panelSpace.BackColor = System.Drawing.Color.Gray;
+            this.panelSpace.Location = new System.Drawing.Point(200, 66);
+            this.panelSpace.Name = "panelSpace";
+            this.panelSpace.Size = new System.Drawing.Size(200, 100);
+            this.panelSpace.TabIndex = 8;
+            // 
+            // panelSpaceLine
+            // 
+            this.panelSpaceLine.BackColor = System.Drawing.Color.Gray;
+            this.panelSpaceLine.Location = new System.Drawing.Point(324, 213);
+            this.panelSpaceLine.Name = "panelSpaceLine";
+            this.panelSpaceLine.Size = new System.Drawing.Size(200, 100);
+            this.panelSpaceLine.TabIndex = 9;
+            // 
+            // timerCps
+            // 
+            this.timerCps.Enabled = true;
+            this.timerCps.Interval = 1000;
+            this.timerCps.Tick += new System.EventHandler(this.timerCps_Tick);
+            // 
+            // timerRGB
+            // 
+            this.timerRGB.Enabled = true;
+            this.timerRGB.Tick += new System.EventHandler(this.timerRGB_Tick);
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.BackColor = System.Drawing.Color.White;
+            this.labelDebug.Location = new System.Drawing.Point(455, 177);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(38, 15);
+            this.labelDebug.TabIndex = 10;
+            this.labelDebug.Text = "label1";
+            // 
+            // comboBoxProcess
+            // 
+            this.comboBoxProcess.FormattingEnabled = true;
+            this.comboBoxProcess.Location = new System.Drawing.Point(108, 200);
+            this.comboBoxProcess.Name = "comboBoxProcess";
+            this.comboBoxProcess.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxProcess.TabIndex = 11;
+            // 
+            // timer500
+            // 
+            this.timer500.Enabled = true;
+            this.timer500.Interval = 500;
+            this.timer500.Tick += new System.EventHandler(this.timer500_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxProcess);
+            this.Controls.Add(this.labelDebug);
+            this.Controls.Add(this.panelSpaceLine);
+            this.Controls.Add(this.panelSpace);
+            this.Controls.Add(this.labelcpsr);
+            this.Controls.Add(this.labelcpsl);
+            this.Controls.Add(this.labelMBR);
+            this.Controls.Add(this.labelMBL);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.labelS);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelW);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelW;
+        private System.Windows.Forms.Timer timerUpdate;
+        private Label labelA;
+        private Label labelS;
+        private Label labelD;
+        private Label labelMBL;
+        private Label labelMBR;
+        private Label labelcpsl;
+        private Label labelcpsr;
+        private Panel panelExplainingBackground;
+        private Label labelExplainingText1;
+        private Button buttonNextExplainingPage;
+        private Panel panelSpace;
+        private Panel panelSpaceLine;
+        private System.Windows.Forms.Timer timerCps;
+        private System.Windows.Forms.Timer timerRGB;
+        private Label labelDebug;
+        private ComboBox comboBoxProcess;
+        private System.Windows.Forms.Timer timer500;
+    }
+}
